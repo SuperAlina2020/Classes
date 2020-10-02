7669ce2a7b0bc839b94dcbb64e67c8da530d2eb7
SuperAlina2020 <alinagithub@gmail.com>
2020-10-02 18:19:06 +0600

    planet and solary sistem

---
+++ planet.py
@@ -0,0 +1,17 @@
+class Planet:
+
+    def __init__(self,name):
+        self.name = name
+
+    def __repr__(self):
+        return  f'Планета: {self.name}!'
+
+
+list_of_planet = ['Earth','Mars','Mercury','Venus','Saturn','Pluton','Jupyter','Neptun']
+
+solary_sistem = []
+for planet in list_of_planet:
+    new_planet = Planet(planet)
+    solary_sistem.append(new_planet)
+
+print(solary_sistem)
