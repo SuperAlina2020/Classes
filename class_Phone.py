65164f2471360f3ff49c81915ffd10a07f90c2a2
SuperAlina2020 <alinagithub@gmail.com>
2020-10-16 19:00:09 +0600

    phone class

--- class_Phone.py
+++ class_Phone.py
@@ -6,7 +6,8 @@ class Phone:
         self.screen = screen
         self.system = system
         self.camera = camera
-
+        self.size = 10
+        self.iscase = False
 
 
     def description(self):
@@ -21,7 +22,6 @@ class Phone:
             print('Your phone has broken!')
 
 
-
     def low_battery(self,battery):
         self.battery = battery
         if self.battery <= 20:
@@ -31,13 +31,32 @@ class Phone:
          print(f'У вашего телефона {self.battery} % зарядки')
 
 
+    def buy_phone(self, cash, price):
+        self.cash = cash
+        self.price = price
+        if self.cash >= self.price:
+            print('Вы можете позволить себе этот телефон')
+        else:
+            print("У вас не хватает денег")
+
+
+    def wear(self,case):
+        if self.size == case:
+            self.iscase = True
+            print(True)
+        else:
+            print(False)
+
 
 class Iphone(Phone):
 
     def __init__(self,model,year,screen,camera,make ='Apple',system='IOS'):
         super().__init__(self,model,year,screen,camera,system)
-
-        self.processor = 'A14'
+        self.apple_id = True
+        self.apple_id_username = ''
+        self.apple_id_password = ''
+        self.apple_id_check_password = ''
+        self.processor = 'A12'
         self.power = 3000
         self.touch_id = True
         self.face_id = True
@@ -45,6 +64,8 @@ class Iphone(Phone):
         self.memory = 256
         self.sim = 1
         self.pin = '1234'
+        self.case = 20
+        self.weight = 200
 
 
     def description(self):
@@ -67,6 +88,28 @@ class Iphone(Phone):
         return f'У вас осталось : {self.battery} %'
 
 
+    def temperature(self,temp):
+        self.battery = 100
+        self.temp = temp
+        if self.temp <= 5:
+            self.battery == 0
+            print('Переохлаждение телефона. Включите позже!')
+
+
+    def broken_glass(self,glass):
+        self.glass = glass
+        if self.glass == 'Broken':
+            print('Change this glass')
+
+
+    def registration(self,username,password,check_password):
+        if password == check_password:
+            self.apple_id_username = username
+            self.apple_id_password = password
+            self.apple_id_check_password = check_password
+            return f'Регистрация прошла успешно'
+        else:
+            print('Повторите попытку')
 
 
 phone1 = Iphone('XS MAX','2018','6.5','12','IOS')
@@ -82,6 +125,8 @@ class Samsung(Phone):
         self.siri = False
         self.memory = 64
         self.owner = owner
+        self.size = 23
+        self.weight = 175
 
 
     def video_play(self,min):
@@ -115,8 +160,27 @@ class Samsung(Phone):
             self.memory -= 5
         elif product == 'online_book':
             self.memory -= 1
-        return f'Память : {self.memory}'
+        return f'Осталось {self.memory} gb памяти'
 
 
-phone2 = Samsung('Galaxy S10','2018','6.4','12','Erjan','Android')
-print(phone2.change_memory('film'))
+    def death(self):
+        if self.camera == 0 and self.battery == 0 and self.screen == 0 and self.touch_id == 0 and self.face_id == 0:
+            print('Ваш телефон ремонту не подлежит')
+        else:
+            print("Обратитесь к специалисту!")
+
+    def take_photo(self):
+        if self.camera > 0:
+            print('Take a picture')
+        else:
+            print('Камера не работает')
+
+
+    def take_video(self):
+        if self.camera > 0:
+            print('Take a video')
+        else:
+            print('Камера не работает')
+
+
+phone2 = Samsung('Galaxy S10','2018','6.4','12','Daniel','Android')
